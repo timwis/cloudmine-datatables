3d10bcc56b76e2391f51d878b09306ea8795df59
Tim Wisniewski <tim@timwis.com>
2012-10-28 01:28:47 -0400

    Added functionality for demo

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var ws = new cloudmine.WebService({
-  appid:  "",
-  apikey: ""
+  appid:  "885393c050dc424aa91ccc8349af22c9",
+  apikey: "d58d4ebc082749c7afad088a5c2416b5"
 });
 
 $(function() {
@@ -11,8 +11,12 @@ $(function() {
 		,bProcessing: true
 		,bServerSide: true
 		,aoColumns: [
-			{sTitle: "Foo", mData: "foo"}
-			,{sTitle: "Bar", mData: "bar"}
+			{sTitle: "Date", mData: "datetime", fnRender: function(o, val) {return display_time(val);}} // Friendly date/time display
+			,{sTitle: "Phone", mData: "phone_number", fnRender: function(o, val) {return val.substring(0,3)+'-'+val.substring(3,6)+'-'+val.substring(6,10);}} // Friendly phone number display
+			,{sTitle: "Survey ID", mData: "survey_id"}
+			,{sTitle: "Color", mData: "color"}
+			,{sTitle: "Food", mData: "food"}
+			,{sTitle: "Music", mData: "music"}
 		]
 		,sAjaxSource: "[__class__ = \"Message\"]" // Use this for the query since it gets passed to fnServerData
 		,fnServerData: dataTablesAjax
@@ -57,4 +61,17 @@ function objToArrayOfObj(data) {
 		}
 	}
 	return results;
+}
+
+function display_time(UNIX_timestamp){
+	var a = new Date(UNIX_timestamp*1000);
+	var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+	var year = a.getFullYear();
+	var month = a.getMonth()+1;
+	var date = a.getDate();
+	var hour = a.getHours();
+	var min = ('0' + a.getMinutes()).slice(-2);
+	var sec = ('0' + a.getSeconds()).slice(-2);
+	var time = month+'/'+date+'/'+year+' '+hour+':'+min+':'+sec ;
+	return time;
 }
